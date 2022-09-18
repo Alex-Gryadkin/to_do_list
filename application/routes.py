f99e08a92dc0372a3990554e69ea54a5f905b365
Alex-Gryadkin <thssfrlrnng@gmail.com>
2022-09-18 15:26:12 +0600

    tests have been added. and other minor changes

--- application/routes.py
+++ application/routes.py
@@ -17,17 +17,3 @@ def index():
 
     to_dos = Task.query.all()
     return render_template('index.html', to_dos=to_dos, form=form)
-
-
-@app.route('/delete/<int:task_id>')
-def delete(task_id):
-    task = Task.query.filter_by(id=task_id).first()
-
-    db.session.delete(task)
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-
-# TODO: when route/ THEN render_template with 'index.html' arg
-
