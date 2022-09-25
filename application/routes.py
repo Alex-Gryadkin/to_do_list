f31419e3ac49941c0e62701efe5bb0ad5afd68b1
Alex-Gryadkin <thssfrlrnng@gmail.com>
2022-09-25 17:08:36 +0600

    now the status button changes a task status

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for
 from application import app
 from application.forms import NewTask
 from application.models import Task
+from application.models import Status
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,8 +23,25 @@ def index():
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     task = Task.query.filter_by(id=task_id).first()
-
     db.session.delete(task)
     db.session.commit()
 
+    return redirect(url_for('index'))
+
+
+@app.route('/change/<int:task_id>')
+def change(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    print(task.status)
+
+    if task.status == Status.to_do:
+        task.status = Status.in_progress
+
+    elif task.status == Status.in_progress:
+        task.status = Status.done
+
+    else:
+        task.status = Status.to_do
+
+    db.session.commit()
     return redirect(url_for('index'))
