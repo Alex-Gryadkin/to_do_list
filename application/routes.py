4d5889d92dc8fcb7397b6409cdd6ed6584d5d44c
Alex-Gryadkin <thssfrlrnng@gmail.com>
2022-09-28 21:43:35 +0600

    the comment from the last PR have been taken into account

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,14 @@
 from application import db
 from flask import render_template, redirect, url_for
 from application import app
-from application.forms import NewTask
+from application.forms import NewTask, LoginForm
 from application.models import Task
 from application.models import Status
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    user = {'username': 'Alex Gryadkin'}
     form = NewTask()
 
     if form.validate_on_submit():
@@ -17,12 +18,12 @@ def index():
         return redirect(url_for('index'))
 
     to_dos = Task.query.all()
-    return render_template('index.html', to_dos=to_dos, form=form)
+    return render_template('index.html', to_dos=to_dos, form=form, user=user)
 
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    task = Task.query.filter_by(id=task_id).first()
+    task = Task.query.get(task_id)
     db.session.delete(task)
     db.session.commit()
 
@@ -31,17 +32,11 @@ def delete(task_id):
 
 @app.route('/change/<int:task_id>')
 def change(task_id):
-    task = Task.query.filter_by(id=task_id).first()
-    print(task.status)
+    task = Task.query.get(task_id)
 
-    if task.status == Status.to_do:
-        task.status = Status.in_progress
-
-    elif task.status == Status.in_progress:
-        task.status = Status.done
-
-    else:
-        task.status = Status.to_do
+    task.status = Status.in_progress if task.status == Status.to_do \
+        else Status.done if task.status == Status.in_progress \
+        else Status.to_do
 
     db.session.commit()
     return redirect(url_for('index'))
